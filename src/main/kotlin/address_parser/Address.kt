9d0828d4de2267e859628423eba98f9b3dd9d02f
CmmSheparD <ustynow.gleb@gmail.com>
2022-03-20 19:09:27 +0300

    Removed redundant member functions' definitions

--- src/main/kotlin/address_parser/Address.kt
+++ src/main/kotlin/address_parser/Address.kt
@@ -2,28 +2,6 @@ package address_parser
 
 data class Address(val index: Int, val city: String, val street: String, val building: Int) {
     override fun toString() = "$index, $city, $street, $building"
-
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (javaClass != other?.javaClass) return false
-
-        other as Address
-
-        if (index != other.index) return false
-        if (city != other.city) return false
-        if (street != other.street) return false
-        if (building != other.building) return false
-
-        return true
-    }
-
-    override fun hashCode(): Int {
-        var result = index
-        result = 31 * result + city.hashCode()
-        result = 31 * result + street.hashCode()
-        result = 31 * result + building
-        return result
-    }
 }
 
 fun parseAddresses(input: String): List<Address> {
