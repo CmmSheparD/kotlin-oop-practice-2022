a1aab5f7708300c2d36b7ce1a467b86781535f07
CmmSheparD <ustynow.gleb@gmail.com>
2022-03-27 18:42:50 +0300

    Added alpha channel support for Color

--- src/main/kotlin/shapes/Color.kt
+++ src/main/kotlin/shapes/Color.kt
@@ -1,9 +1,10 @@
 package shapes
 
-data class Color(val red: UByte, val green: UByte, val blue: UByte) {
+data class Color(val red: UByte, val green: UByte, val blue: UByte, val alpha: UByte) {
     constructor(value: String) : this(
         value.slice(0..1).toUByte(16),
         value.slice(2..3).toUByte(16),
         value.slice(4..5).toUByte(16),
+        value.slice(6..7).toUByte(16),
     )
 }
--- src/test/kotlin/shapes/ColorTest.kt
+++ src/test/kotlin/shapes/ColorTest.kt
@@ -7,23 +7,25 @@ import kotlin.test.assertFailsWith
 internal class ColorTest {
     @Test
     fun createColorTest() {
-        val color = Color(0U, 2U, 4U)
+        val color = Color(0U, 2U, 4U, 6U)
         assertEquals(0.toUByte(), color.red)
         assertEquals(2.toUByte(), color.green)
         assertEquals(4.toUByte(), color.blue)
+        assertEquals(6.toUByte(), color.alpha)
     }
 
     @Test
     fun createColorFromStringTest() {
-        val color = Color("12ffA5")
+        val color = Color("12f0A5ff")
         assertEquals(0x12.toUByte(), color.red)
-        assertEquals(0xff.toUByte(), color.green)
+        assertEquals(0xf0.toUByte(), color.green)
         assertEquals(0xa5.toUByte(), color.blue)
+        assertEquals(0xff.toUByte(), color.alpha)
     }
 
     @Test
     fun createColorFromInvalidString() {
-        assertFailsWith(NumberFormatException::class, block = { Color("invalid") })
+        assertFailsWith(NumberFormatException::class, block = { Color("invalid string") })
     }
 
     @Test
